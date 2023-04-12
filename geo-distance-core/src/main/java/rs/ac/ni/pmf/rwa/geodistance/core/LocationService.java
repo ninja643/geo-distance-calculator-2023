32fc59fb93aa65217494637aff7a7ee1ab2e9d70
Marko Milosevic <markom@niri.rs>
2023-04-12 14:50:07 +0200

    Reorganize application into modules

--- geo-distance-core/src/main/java/rs/ac/ni/pmf/rwa/geodistance/core/LocationService.java
+++ geo-distance-core/src/main/java/rs/ac/ni/pmf/rwa/geodistance/core/LocationService.java
@@ -14,4 +14,5 @@ public class LocationService
 	{
 		return locationProvider.getLocations();
 	}
+
 }
--- geo-distance-webapp/src/main/java/rs/ac/ni/pmf/rwa/geodistance/rest/LocationRestController.java
+++ geo-distance-webapp/src/main/java/rs/ac/ni/pmf/rwa/geodistance/rest/LocationRestController.java
@@ -4,17 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rs.ac.ni.pmf.rwa.geodistance.core.LocationService;
-import rs.ac.ni.pmf.rwa.geodistance.core.model.Location;
 import rs.ac.ni.pmf.rwa.geodistance.rest.dto.LocationDTO;
 import rs.ac.ni.pmf.rwa.geodistance.rest.mapper.LocationMapper;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-public class LocationRestService
+public class LocationRestController
 {
 	private final LocationService locationService;
 	private final LocationMapper locationMapper;
